c6f3e762e0a358ed12b93874a988c7296ae2d5b0
nalberga <nicolas.alberga@gmail.com>
2020-03-05 16:53:41 -0500

    new commit

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const bcrypt = require('bcrypt');
 
 const Sequelize = require('sequelize');
 const UserModel = require('./database/models/user');
+const ProductModel = require('./database/models/product');
+const OrderModel = require('./database/models/order');
+const OrderProductModel = require('./database/models/order_product');
 
 const connectionString = `postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`
 const sequelize = new Sequelize(process.env.DATABASE_URL || connectionString, {
@@ -35,6 +38,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || connectionString, {
 console.log(connectionString);
 
 const Users = UserModel(sequelize, Sequelize);
+const Products = ProductModel(sequelize, Sequelize);
+const Orders = OrderModel(sequelize, Sequelize);
+const OrderProducts = OrderProductModel(sequelize, Sequelize);
 
 const app = express();
 
@@ -44,7 +50,7 @@ app.use(cors());
 app.use(express.static('public'));
 
 // API get all users
-app.get('/api/users/all', function (req, res) {
+app.get('/api/users/', (req, res) => {
 
     Users.findAll().then((results) => {
         res.setHeader('Content-Type', 'application/json');
@@ -57,7 +63,7 @@ app.get('/api/users/all', function (req, res) {
 });
 
 // API get target user
-app.get('/api/users/:id', function (req, res) {
+app.get('/api/users/:id', (req, res) => {
 
     let id = req.params.id;
 
@@ -70,7 +76,7 @@ app.get('/api/users/:id', function (req, res) {
             res.status(434).send('User does not exist is DB');
         }
 
-    }).catch(function (e) {
+    }).catch((e) => {
         console.log(e);
         res.status(434).send('error retrieving info on target User');
     })
@@ -78,13 +84,13 @@ app.get('/api/users/:id', function (req, res) {
 });
 
 // API add user
-app.post('/api/users/register', function (req, res) {
+app.post('/api/users/register', (req, res) => {
 
     //default admin to false if req.body.admin is null
     if (req.body.admin == null) {
         req.body.admin = false;
     }
-    
+
     const data = {
         f_name: req.body.f_name.trim(),
         l_name: req.body.l_name.trim(),
@@ -94,14 +100,14 @@ app.post('/api/users/register', function (req, res) {
         admin: req.body.admin,
     };
 
-    
+
     if (data.f_name && data.l_name && data.email && data.password) {
 
         var salt = bcrypt.genSaltSync(10);
         var hash = bcrypt.hashSync(data.password, salt);
         data.password = hash;
 
-        Users.create(data).then(function (user) {
+        Users.create(data).then((user) => {
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(user));
         });
@@ -116,7 +122,7 @@ app.post('/api/users/register', function (req, res) {
 
 // API update a target user's info
 app.put('/api/users/:id', function (req, res) {
-    
+
     const data = {
 
         id: req.params.id,
@@ -164,31 +170,26 @@ app.put('/api/users/:id', function (req, res) {
 });
 
 // API delete target User
-app.delete('/api/users/:id', function (req, res) {
+app.delete('/api/users/:id', (req, res) => {
 
     const id = req.params.id;
 
-    Users.findOne({ where: { id: id } }).then(results => {
+    Users.findOne({ where: { id: id } }).then(user => {
 
-        if (results) {
-            
-            const targetUser = JSON.stringify(results);
+        user.destroy().then(() => {
 
-            Users.destroy({ where: { id: id } }).then(function (user) {
-                
-                res.setHeader('Content-Type', 'application/json');
-                res.end(targetUser);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(user));
 
-            }).catch(function (e) {
+        }).catch((e) => {
 
-                res.status(434).send('unable to delete User')
+            console.log(e);
+            res.status(434).send('unable to delete User')
 
-            })
+        })
 
-        } 
+    }).catch((e) => {
 
-    }).catch(function (e) {
-        
         console.log(e);
         res.status(434).send('error retrieving info on target User');
 
@@ -196,5 +197,448 @@ app.delete('/api/users/:id', function (req, res) {
 
 });
 
-app.listen(3000);
+// API get all products
+app.get('/api/products/', function (req, res) {
+
+    Products.findAll().then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Products');
+    })
+
+});
+
+// API get all products by type
+app.get('/api/products/type/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Products.findAll({ where: { type_id: id } }).then((results) => {
+
+        if (results.length != 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('No products found belonging to this type!');
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Products of this type');
+    })
+
+});
+
+// API get all products by category
+app.get('/api/products/category/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Products.findAll({ where: { category_id: id } }).then((results) => {
+
+        if (results.length != 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('No products found belonging to this category!');
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Products of this category');
+    })
+
+});
+
+// API get target product
+app.get('/api/products/:id', function (req, res) {
+
+    let id = req.params.id;
+
+    Products.findOne({ where: { id: id } }).then(results => {
+
+        if (results) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('Product not found');
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('error retrieving info on target Product');
+    })
+
+});
+
+// API add product
+app.post('/api/products/register', function (req, res) {
+
+    const data = {
+
+        name: req.body.name,
+        type_id: req.body.type_id,
+        category_id: req.body.category_id,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        price: req.body.price,
+        image_url: req.body.image_url
+
+    };
+
+
+    if (data.name && data.type_id && data.category_id && data.description && data.quantity && data.price && data.image_url) {
+
+        Products.create(data).then(product => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(product));
+        }).catch((e) => {
+            console.log(e);
+            res.status(434).send('error registering Product');
+        })
+
+    } else {
+
+        res.status(434).send('All fields are required to register.')
+
+    }
+
+});
+
+// API update a target product's info
+app.put('/api/products/:id', function (req, res) {
+
+    const data = {
+
+        id: req.params.id,
+        name: req.body.name,
+        type_id: req.body.type_id,
+        category_id: req.body.category_id,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        price: req.body.price,
+        image_url: req.body.image_url
+
+    };
+
+    Products.findOne({ where: { id: data.id } }).then(product => {
+
+        product.update({
+
+            name: data.name,
+            type_id: data.type_id,
+            category_id: data.category_id,
+            description: data.description,
+            quantity: data.quantity,
+            price: data.price,
+            image_url: data.image_url,
+
+        }).then(newData => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(newData));
+        }).catch((e) => {
+            res.status(434).send('unable to update Product')
+        })
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(434).send(`unable to find Product ${data.id}`)
+
+    })
+
+});
+
+// API delete target Product
+app.delete('/api/products/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    Products.findOne({ where: { id: id } }).then(product => {
+
+        product.destroy().then(() => {
+
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(product));
+
+        }).catch((e) => {
+
+            console.log(e);
+            res.status(434).send('unable to delete Product')
+
+        })
+
+    }).catch((e) => {
+
+        console.log(e);
+        res.status(434).send('error retrieving info on target Product');
+
+    })
+
+});
+
+// API get all orders
+app.get('/api/orders/', (req, res) => {
+
+    Orders.findAll().then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Orders');
+    })
+
+});
+
+// API get target order
+app.get('/api/orders/:id', function (req, res) {
+
+    let id = req.params.id;
+
+    Orders.findOne({ where: { id: id } }).then(results => {
+
+        if (results) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('Order does not exist is DB');
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('error retrieving info on target Order');
+    })
+
+});
+
+// API get all orders made by target User
+app.get('/api/orders/user/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Orders.findAll({ where: { user_id: id } }).then((results) => {
+
+        if (results.length != 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send(`No orders have been made by user ${id}!`);
+        }
+
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('User does not exist');
+    })
+
+});
+
+// Register an Order
+app.post('/api/orders/register', function (req, res) {
+
+    const data = {
+
+        user_id: req.body.user_id,
+        purchase_date: new Date(),
+        quantity: req.body.quantity,
+        price: req.body.price
+
+    };
+
+
+    if (data.user_id && data.purchase_date && data.quantity && data.price) {
+
+        Orders.create(data).then(order => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(order));
+        }).catch((e) => {
+            console.log(e);
+            res.status(434).send('error registering Order');
+        })
+
+    } else {
+
+        res.status(434).send('All fields are required to register an Order.');
+
+    }
+
+});
+
+// API update a target order's info
+app.put('/api/orders/:id', function (req, res) {
+
+    const data = {
+
+        id: req.params.id,
+        user_id: req.body.user_id,
+        quantity: req.body.quantity,
+        price: req.body.price
+
+    };
+
+    Orders.findOne({ where: { id: data.id } }).then(order => {
+
+        order.update({
+
+            user_id: data.user_id,
+            quantity: data.quantity,
+            price: data.price
+
+        }).then(newData => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(newData));
+        }).catch((e) => {
+            res.status(434).send('unable to update Order')
+        })
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(434).send(`unable to find Order ${data.id}`)
+
+    })
+
+});
+
+// API delete target Order
+app.delete('/api/orders/:id', function (req, res) {
+
+    const id = req.params.id;
+
+    Orders.findOne({ where: { id: id } }).then(order => {
+
+        order.destroy().then(() => {
+
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(order));
+
+        }).catch((e) => {
+
+            res.status(434).send('unable to delete Order')
+
+        })
+
+    }).catch((e) => {
+
+        console.log(e);
+        res.status(434).send('error retrieving info on target Order');
+
+    })
+
+});
+
+//API get all order products
+app.get('/api/order-products/', (req, res) => {
+
+    OrderProducts.findAll().then((results) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(results));
+    }).catch(function (e) {
+        console.log(e);
+        res.status(434).send('Error retrieving Order Products');
+    })
+
+});
+
+// API get target order product
+app.get('/api/order-products/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    OrderProducts.findOne({ where: { id: id } }).then(results => {
+
+        if (results) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('Order product does not exist is DB');
+        }
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(434).send('error retrieving info on target Order product');
+    })
+
+});
+
+// API get all products of target order
+app.get('/api/order-products/order/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    OrderProducts.findAll({ where: { order_id: id } }).then((results) => {
+
+        if (results.length != 0) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(results));
+        } else {
+            res.status(434).send('No products found belonging to this order!');
+        }
+
+    }).catch((e) => {
+        console.log(e);
+        res.status(434).send('Error retrieving Products of this order');
+    })
+
+});
+
+// API get all order products of target user
+app.get('/api/order-products/user/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    Orders.findAll({ where: { user_id: id } }).then((orders) => {
+
+        if (orders.length != 0) {
+
+            let userOrderProducts = [];
+
+            let promise = new Promise(() => {
+                
+                orders.forEach(order => {
+
+                    OrderProducts.findAll({
+
+                        where: { order_id: order.id }
+
+                    }).then(results => {
+
+                        results.forEach(product => userOrderProducts.push(product))
+
+                    }).catch((e) => {
+
+                        console.log(e);
+                        res.status(434).send('Error retrieving Products of this order');
+
+                    });
+
+                })
+                .then(() => {
+
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(userOrderProducts));
+
+                })
+                .catch((e) => {
+                    console.log(e);
+                    res.status(434).send('Error retrieving Products of this User');
+                })
+            });
+
+        } else {
+
+            console.log(e);
+            res.status(434).send('No products found belonging to this user!');
+
+        }
+
+    }).catch((e) => {
+
+        console.log(e);
+        res.status(434).send('Error retrieving Products belonging to this user');
+
+    });
+
+});
+
+app.listen(3002);
 console.log('Drinks-R-Us API is running');
